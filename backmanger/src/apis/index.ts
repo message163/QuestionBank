877e87b4df5a773b672253a26a0f64ae9a53df59
xiaoMan <1195566313@qq.com>
2024-08-14 15:20:09 +0800

    dist env log

--- backmanger/src/apis/index.ts
+++ backmanger/src/apis/index.ts
@@ -4,6 +4,8 @@ const env = import.meta.env
 //1. /api开头的接口是提供增删改差的核心逻辑接口
 //2. /fs开头的接口是提供文件读写的接口
 
+console.log(env)
+
 const api = axios.create({
     baseURL: env.MODE === 'development' ? 'http://localhost:9002' : '',
     timeout: 5000
